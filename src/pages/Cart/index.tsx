0c12ecabe60eefd3a6e581d168447a8fea31739c
Samuel Sant Anna <samuelsantaanna@gmail.com>
2021-05-30 17:49:02 -0300

    working

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -88,7 +88,7 @@ const Cart = (): JSX.Element => {
                     <button
                       type="button"
                       data-testid="increment-product"
-                      onClick={() => handleProductIncrement()}
+                      onClick={() => handleProductIncrement(product)}
                     >
                       <MdAddCircleOutline size={20} />
                     </button>
